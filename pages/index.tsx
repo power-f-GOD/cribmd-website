4b3c3c264f1b4374ab8f6f9e3e8ddd8021c6796c
Michael Ngiri <michaelngiri@gmail.com>
2021-03-08 00:08:59 +0100

    finish up project setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { GetServerSideProps, NextPage } from 'next'
+import { NextPage } from 'next'
 
 interface Props {
   launch: {
@@ -9,8 +9,8 @@ interface Props {
     rocket: string
   }
 }
-const IndexPage: NextPage<Props> = ({ launch }) => {
-  const date = new Date(launch.timestamp)
+const IndexPage: NextPage<Props> = () => {
+  //   const date = new Date(launch.timestamp)
   return (
     <main>
       <Head>
@@ -19,28 +19,28 @@ const IndexPage: NextPage<Props> = ({ launch }) => {
       </Head>
       <h1>welcome to Studiare</h1>
       <img src="studiare-logo.jpg" alt="" />
-      <h2>Next SpaceX Launch: {launch.mission}</h2>
+      {/* <h2>Next SpaceX Launch: {launch.mission}</h2>
       <p>
         {launch.rocket} will take off from {launch.site} on {date.toDateString()}
-      </p>
+      </p> */}
     </main>
   )
 }
 export default IndexPage
 
-// sample data fetching from API
+// use getStaticProps for SSG
 
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const response = await fetch('https://api.spacexdata.com/v3/launches/next')
-  const nextLaunch = await response.json()
-  return {
-    props: {
-      launch: {
-        mission: nextLaunch.mission_name,
-        site: nextLaunch.launch_site.site_name_long,
-        timestamp: nextLaunch.launch_date_unix * 1000,
-        rocket: nextLaunch.rocket.rocket_name,
-      },
-    },
-  }
-}
+// export const getServerSideProps: GetServerSideProps<Props> = async () => {
+//   const response = await fetch('https://api.spacexdata.com/v3/launches/next')
+//   const nextLaunch = await response.json()
+//   return {
+//     props: {
+//       launch: {
+//         mission: nextLaunch.mission_name,
+//         site: nextLaunch.launch_site.site_name_long,
+//         timestamp: nextLaunch.launch_date_unix * 1000,
+//         rocket: nextLaunch.rocket.rocket_name,
+//       },
+//     },
+//   }
+// }
