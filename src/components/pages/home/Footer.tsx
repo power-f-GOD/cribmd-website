3ffd79b97b6c80946d949c430acf04fef0ffaf42
moyinobey <mobey@appzonegroup.com>
2021-07-22 20:22:58 +0100

    changed footer to box

--- src/components/pages/home/Footer.tsx
+++ src/components/pages/home/Footer.tsx
@@ -1,10 +1,12 @@
 // Please, note that this Footer is different from the global (or app) AppFooter that is common to all pages. This is the base/exit section of this page just before the AppFooter
 
+import { Box } from 'src/components/shared';
+
 const Footer = (): JSX.Element => {
   return (
-    <footer>
+    <Box as="footer">
       <h6>This is the Footer component!</h6>
-    </footer>
+    </Box>
   );
 };
 
