2af18b39cfa0bf6735ef6f9709954a053dc69cc9
moyinobey <mobey@appzonegroup.com>
2021-08-12 05:37:19 +0100

    implemented media slider for about us, in the media page

---
+++ src/components/pages/about/in-the-media/data.ts
@@ -0,0 +1,27 @@
+export const mediaItems = [
+  {
+    imageName: 'algrowithm',
+    header: 'Techawk',
+    content: 'Nigerian healthcare Startup, CribMD secures $2.6m funding...'
+  },
+  {
+    imageName: 'andaman',
+    header: 'Spotify',
+    content: 'CribMD: Nigerian-founded telemedicine health tech startup '
+  },
+  {
+    imageName: 'bora',
+    header: 'Techpoint Africa',
+    content: 'Nigerian Healthcare Startup, CribMD, closes $2.6m  seed round'
+  },
+  {
+    imageName: 'lorna',
+    header: 'Inc',
+    content: 'See our company profile,Nigerian-founded  health tech startup  '
+  },
+  {
+    imageName: 'ucla',
+    header: 'test',
+    content: 'in Healthcare Startup, CribMD, closes $2.6m seed rounds'
+  }
+];
