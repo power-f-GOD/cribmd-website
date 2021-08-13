e0bb71ca1420d03da77f6178016fe6d8b04c7ee2
power-f-GOD <sundaypower9@gmail.com>
2021-08-13 13:43:00 +0100

    Add carousel-like feature to Gallery photos.

--- src/components/pages/about/in-the-media/Main.Gallery.tsx
+++ src/components/pages/about/in-the-media/Main.Gallery.tsx
@@ -1,18 +1,35 @@
+import { memo, useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+
 import { Box, Img, RevealOnScroll } from 'src/components/shared';
-// import { useState, useCallback, useEffect } from 'react';
-// import { GetImage, interval } from 'src/utils';
 import S from 'src/styles/pages/about/in-the-media/Main.module.scss';
-// import { mediaItems } from './data';
-import { memo } from 'react';
-import { GetImage } from 'src/utils';
+import { GetImage, interval } from 'src/utils';
+
+const images = Array(12).fill({
+  title: '',
+  name: 'image',
+  description: 'A short description about the event or people in the Image.'
+});
+let clearPhotoInterval = false;
 
 const MainGallery = (): JSX.Element => {
-  const images = Array(12).fill({
-    title: '',
-    name: 'image',
-    description: 'A short description about the event or people in the Image.'
-  });
+  const [activePhotoIndex, setActivePhotoIndex] = useState(0);
+
+  useEffect(() => {
+    interval(
+      () => {
+        setActivePhotoIndex((index) => {
+          return index === images.length - 1 ? 0 : index + 1;
+        });
+      },
+      3000,
+      () => clearPhotoInterval
+    );
+
+    return () => {
+      clearPhotoInterval = true;
+    };
+  }, []);
 
   return (
     <Container as="section" className={`${S.Gallery} mt-5`}>
@@ -24,7 +41,11 @@ const MainGallery = (): JSX.Element => {
 
       <RevealOnScroll className={`${S.galleryGrid} mb-3`} animName="fadeInDown" easing="ease">
         {images.slice(0, 4).map(({ name, description }, i) => (
-          <Box className={`${S.galleryGridItem}`} key={i}>
+          <Box
+            className={`${S.galleryGridItem} ${
+              activePhotoIndex === i ? S.galleryGridItemActive : ''
+            }`}
+            key={i}>
             <Img src={GetImage.gallery(name)} />
             <Box as="p">
               <Box as="span">{description}</Box>
@@ -35,7 +56,11 @@ const MainGallery = (): JSX.Element => {
 
       <RevealOnScroll className={`${S.galleryGrid} mb-3`} animName="fadeInLeft" easing="ease">
         {images.slice(4, 8).map(({ name, description }, i) => (
-          <Box className={`${S.galleryGridItem}`} key={i}>
+          <Box
+            className={`${S.galleryGridItem}  ${
+              activePhotoIndex === 4 + i ? S.galleryGridItemActive : ''
+            }`}
+            key={i}>
             <Img src={GetImage.gallery(name)} />
             <Box as="p">
               <Box as="span">{description}</Box>
@@ -46,7 +71,11 @@ const MainGallery = (): JSX.Element => {
 
       <RevealOnScroll className={`${S.galleryGrid}`} animName="fadeInUp" easing="ease">
         {images.slice(8, 12).map(({ name, description }, i) => (
-          <Box className={`${S.galleryGridItem}`} key={i}>
+          <Box
+            className={`${S.galleryGridItem}  ${
+              activePhotoIndex === 8 + i ? S.galleryGridItemActive : ''
+            }`}
+            key={i}>
             <Img src={GetImage.gallery(name)} />
             <Box as="p">
               <Box as="span">{description}</Box>
