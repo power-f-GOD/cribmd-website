92bfa08494f176cbf56f0fb2750e6d85140764c0
Damola Adewunmi <damolasd@gmail.com>
2021-08-29 00:18:37 +0100

    Installed react-gtm-module and its type' Initialized the Tag Manager in the AppHead component

--- src/components/AppHead.tsx
+++ src/components/AppHead.tsx
@@ -1,8 +1,23 @@
 // Note: This is AppHead not Header: <head> not <header>.
 
 import Head from 'next/head';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+
+// const tagManagerArgs = {
+//   gtmId: 'UA-205113192-1',
+//   dataLayer: {
+//     js: new Date(),
+//     config: 'UA-205113192-1'
+//   }
+// };
+
+// TagManager.initialize(tagManagerArgs);
 
 const AppHead = ({ title }: { title?: string }): JSX.Element => {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'UA-205113192-1' });
+  }, []);
   return (
     <Head>
       <title>CribMD | {title || 'Home'}</title>
